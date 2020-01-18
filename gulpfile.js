ddc3b5794a67d0f16746698a893dd6591319000e
icho-web <54773154+icho-web@users.noreply.github.com>
2020-01-18 20:42:49 +0500

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -105,7 +105,7 @@ gulp.task("css", function () {
 
 gulp.task("server", function () {
   server.init({
-    server: "build/",
+    server: "source/",
     notify: false,
     open: true,
     cors: true,
